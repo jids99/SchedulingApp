a1cba8c2c576b5a3afd8853a01cb23b0407ad594
Israel Santos <santosjohnisrael@gmail.com>
2025-08-02 03:47:52 +0800

    event calendar

--- scheduling/src/App.tsx
+++ scheduling/src/App.tsx
@@ -22,31 +22,97 @@ import AddSchedule from "./components/AddSchedule"
 
 import { supabase } from './supabaseClient';
 import { useEffect, useState } from "react"
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+
+import {
+  Calendar,
+  CalendarCurrentDate,
+  CalendarDayView,
+  CalendarMonthView,
+  CalendarNextTrigger,
+  CalendarPrevTrigger,
+  CalendarTodayTrigger,
+  CalendarViewTrigger,
+  CalendarWeekView,
+  CalendarYearView,
+} from '@/components/ui/full-calendar';
+
 
 type Schedule = {
   schedule_id: number;
   name: string;
   event_name: string;
   schedule_date: string;
+};
 
+// 1. Define event type
+export type CalendarEvent = {
+  id: string;
+  start: Date;
+  end: Date;
+  title: string;
+  color: string;
+};
+
+// 2. Define Calendar props
+export type CalendarProps = {
+  events: CalendarEvent[];
+  children?: React.ReactNode;
 };
 
 export default function App() {
 
   const [schedules, setSchedules] = useState<Schedule[]>([]);
+  const [eventsArr, setEventsArr] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchSchedules = async () => {
+    async function fetchSchedules () {
       const { data, error } = await supabase
         .from<'schedules',Schedule>('schedules')
         .select();
 
-      if (error) console.error('Error fetching:', error);
-      else setSchedules(data || []);
-    };
+      if (error) {
+          console.error('Error fetching:', error);
+        } else {
+          setSchedules(data || []);
+          const selected = data.map(({ schedule_id, name, schedule_date }) => (
+            { 
+              id: String(schedule_id), 
+              start: new Date(schedule_date),
+              end: new Date(schedule_date),
+              title: name,
+              color: "default"
+            }
+          ));
+          console.log('selected ',selected);
+          const y = [...selected];
+          setEventsArr(y);
+          setLoading(false);
+        }
+    }
 
     fetchSchedules();
   }, []);
+  
+  const x: CalendarEvent[] = [
+            {
+              id: '1',
+              start: new Date('2025-08-02'),
+              end: new Date('2025-08-02'),
+              title: 'Meeting with John',
+              color: 'pink',
+            },
+            {
+              id: '2',
+              start: new Date('2025-08-26'),
+              end: new Date('2025-08-26'),
+              title: 'Project Review',
+              color: 'blue',
+            },
+          ];
+
+    if (loading) return <div>Loading...</div>;
 
   return (
     <>
@@ -77,6 +143,64 @@ export default function App() {
           </Dialog>
         </div>
 
+    <Calendar
+          events = {eventsArr}
+        >
+
+      <div className="h-svh w-full p-14 flex flex-col">
+        <div className="flex px-6 items-center gap-2 mb-6">
+          <CalendarViewTrigger
+            className="aria-[current=true]:bg-accent"
+            view="day"
+          >
+            Day
+          </CalendarViewTrigger>
+          <CalendarViewTrigger
+            view="week"
+            className="aria-[current=true]:bg-accent"
+          >
+            Week
+          </CalendarViewTrigger>
+          <CalendarViewTrigger
+            view="month"
+            className="aria-[current=true]:bg-accent"
+          >
+            Month
+          </CalendarViewTrigger>
+          <CalendarViewTrigger
+            view="year"
+            className="aria-[current=true]:bg-accent"
+          >
+            Year
+          </CalendarViewTrigger>
+
+          <span className="flex-1" />
+
+          <CalendarCurrentDate />
+
+          <CalendarPrevTrigger>
+            <ChevronLeft size={20} />
+            <span className="sr-only">Previous</span>
+          </CalendarPrevTrigger>
+
+          <CalendarTodayTrigger>Today</CalendarTodayTrigger>
+
+          <CalendarNextTrigger>
+            <ChevronRight size={20} />
+            <span className="sr-only">Next</span>
+          </CalendarNextTrigger>
+
+        </div>
+
+        <div className="flex-1 px-6 overflow-hidden">
+          <CalendarDayView />
+          <CalendarWeekView />
+          <CalendarMonthView />
+          <CalendarYearView />
+        </div>
+      </div>
+    </Calendar>
+
       <Table>
       {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
       <TableHeader>
@@ -88,7 +212,8 @@ export default function App() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {schedules.map(schedule => (
+        {schedules.map(schedule => 
+        (
           <TableRow key={schedule.schedule_id}>
             <TableCell> {schedule.schedule_id}</TableCell>
             <TableCell> {schedule.name}</TableCell>
